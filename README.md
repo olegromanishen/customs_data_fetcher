992eb8da04b84ebcce55e18c58ec41c49c1301ba
o.romanishen <o.romanishen@yandex.ru>
2024-06-22 17:34:25 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# customs_data_fetcher
+# Customs Data Fetcher
+
+This package fetches customs data from an API and saves it to a CSV file.
+
+## Installation
+
+To install and set up the project, follow these steps:
+
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/olegromanishen/customs_data_fetcher.git
+    cd <repository_directory>
+    ```
+
+2. **Install Poetry:**
+
+    Poetry is a dependency manager for Python. If you haven't installed Poetry yet, you can do so by following the instructions on the [official website](https://python-poetry.org/docs/#installation).
+
+3. **Install the package using Poetry:**
+
+    ```bash
+    poetry install
+    ```
+
+## Usage
+
+To fetch the data and save it to a CSV file, you can run the script provided in the package.
+
+1. **Default Usage:**
+
+    By default, the data will be saved to `customs_data.csv` in the current directory.
+
+    ```bash
+    poetry run fetch_customs_data
+    ```
+
+2. **Specify Output Path:**
+
+    If you want to save the data to a specific file path, you can use the `--output` argument:
+
+    ```bash
+    poetry run fetch_customs_data --output /path/to/output.csv
+    ```
+
---
+++ customs_data_fetcher/main.py
@@ -0,0 +1,77 @@
+import aiohttp
+import asyncio
+import pandas as pd
+from aiofiles import open as aioopen
+from io import StringIO
+from typing import Any, Dict, List, Optional
+import argparse
+
+
+# Константы API и файла
+API_URL = "http://5.159.103.79:4000/api/v1/logs"
+DEFAULT_OUTPUT_CSV = "customs_data.csv"
+RATE_LIMIT_DELAY = 180  # задержка в секундах
+HEADERS = {'accept': 'application/json'}
+
+
+async def fetch_page(session: aiohttp.ClientSession,
+                     page: int) -> Optional[List[Dict[str, Any]]]:
+    try:
+        async with session.get(API_URL, headers=HEADERS,
+                               params={'page': page}) as response:
+            if response.status == 429:
+                print(
+                    f"Rate limit exceeded. Waiting for {RATE_LIMIT_DELAY} seconds."
+                )
+                await asyncio.sleep(RATE_LIMIT_DELAY)
+                return await fetch_page(session, page)
+            if response.status != 200:
+                response.raise_for_status()
+            data = await response.json()
+            return data.get('items', [])
+    except Exception as e:
+        print(f"Failed to fetch page {page}: {str(e)}")
+        return None
+
+
+async def fetch_and_save_data(output_file: str) -> None:
+    page: int = 1
+
+    async with aiohttp.ClientSession() as session:
+        async with aioopen(output_file, 'w') as f:
+            header_written = False
+            while True:
+                data = await fetch_page(session, page)
+                if not data:
+                    break
+                df = pd.DataFrame(data)
+
+                buffer = StringIO()
+                df.to_csv(buffer,
+                          sep='\t',
+                          index=False,
+                          header=not header_written)
+
+                await f.write(buffer.getvalue())
+
+                if not header_written:
+                    header_written = True
+                page += 1
+
+
+async def main(output_file: str) -> None:
+    print("Fetching data from API...")
+    await fetch_and_save_data(output_file)
+    print(f"Data saved to {output_file}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Fetch customs data and save to CSV.")
+    parser.add_argument("--output",
+                        type=str,
+                        default=DEFAULT_OUTPUT_CSV,
+                        help="Output CSV file path")
+    args = parser.parse_args()
+
+    asyncio.run(main(args.output))
